bd7dacae42e0682b2e74286ab7ce7a56abedc5c6
dustin0116 <dustin0116@gmail.com>
2020-02-23 13:36:42 -0800

    Shows user info to confirm the correct state

--- src/features/Dashboard/index.js
+++ src/features/Dashboard/index.js
@@ -3,11 +3,13 @@ import { useProfileProvider } from 'contexts/profile';
 import Logout from 'components/Logout';
 
 const Dashboard = () => {
+  const { state } = useProfileProvider();
   const { state: { name: { first } } } = useProfileProvider();
 
   return (
     <div className="dashboard">
       <h1>{`Welcome ${first}!`}</h1>
+      <h2>{JSON.stringify(state)}</h2>
       <p>Login Successful.</p>
       <Logout />
     </div>
